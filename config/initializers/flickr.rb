64bf2928b781a362c5e396c6ae579aa961d54476
Johannes Kloch Badsberg <jk.badsberg@gmail.com>
2015-03-16 10:21:29 +0100

    configured flickr auth

--- config/initializers/flickr.rb
+++ config/initializers/flickr.rb
@@ -1,6 +1,6 @@
 # Please get your own API key for setting up this.
 # You can request it at
 # https://www.flickr.com/services/api/misc.api_keys.html
-FLICKR_KEY=''
-FLICKR_SECRET=''
+FLICKR_KEY=ENV['FLICKR_KEY']
+FLICKR_SECRET=ENV['FLICKR_SECRET']
 
